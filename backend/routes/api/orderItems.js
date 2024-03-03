d0d9eb986c640a22e1164e353b0e6efb2829b41e
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-03-02 18:19:04 -0800

    Update orderItems.js

--- backend/routes/api/orderItems.js
+++ backend/routes/api/orderItems.js
@@ -10,6 +10,40 @@ const {
 } = require("../../db/models");
 
 const router = express.Router();
-// GEt all items
 
+// GET all OrderItems
+router.get("/", async (req, res) => {
+  try {
+    let result = await readAllEntry("OrderItem");
+    res.status(200).json({ "OrderItems:": result });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+// GET OrderItem by ID
+router.get("/:orderItemId", async (req, res) => {
+  try {
+    const review = await readEntryById("OrderItem", req.params.reviewId);
+    if (!review) {
+      return res.status(404).json({ message: "OrderItem not found" });
+    }
+    return res.status(200).json(review);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+// GET OrderItem by Order
+router.get("/:orderId", async (req, res) => {
+  try {
+    const review = await readEntryByAggerate("OrderItem", {
+      where: { orderId: req.params.orderId },
+    });
+    if (!review) {
+      return res.status(404).json({ message: "OrderItems not found" });
+    }
+    return res.status(200).json(review);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 module.exports = router;
