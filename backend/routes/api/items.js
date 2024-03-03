4ed20ea0a3c3790af3bacf4f7d1b2b04db09e550
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-03-02 18:12:00 -0800

    Update items.js

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -22,5 +22,32 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+// GET Item by ID
+router.get("/:itemId", async (req, res) => {
+  try {
+    const order = await readEntryById("Item", req.params.itemId);
+    if (!order) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+    return res.status(200).json(order);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
+// GET All Items of a certain type
+router.get("/", async (req, res) => {
+  try {
+    const type = req.body.type;
+    const items = await readEntryByAggerate("Item", {
+      where: { type: type },
+    });
+    if (!items) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+    return res.status(200).json(items);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 module.exports = router;
