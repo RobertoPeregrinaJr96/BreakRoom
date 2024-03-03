b34f7ef5bc285414491746063e6a4653bbcfcf16
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-03-02 17:07:32 -0800

    created read function for orders and review routes

--- backend/routes/api/orders.js
+++ backend/routes/api/orders.js
@@ -14,9 +14,26 @@ const {
 
 const router = express.Router();
 
-// GET all ShoppingCart
+// GET all Order
 router.get("/", async (req, res) => {
- 
+  try {
+    let result = await readAllEntry("Order");
+    res.status(200).json({ "Orders:": result });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+// GET Order by ID
+router.get("/:orderId", async (req, res) => {
+  try {
+    const order = await readEntryById("Order", req.params.orderId);
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+    return res.status(200).json(order);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -10,4 +10,41 @@ const {
 } = require("../../db/models");
 const router = express.Router();
 
+// GET all Review
+router.get("/", async (req, res) => {
+  try {
+    let result = await readAllEntry("Review");
+    res.status(200).json({ "Reviews:": result });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+// GET Review by ID
+router.get("/:reviewId", async (req, res) => {
+  try {
+    const review = await readEntryById("Review", req.params.reviewId);
+    if (!review) {
+      return res.status(404).json({ message: "Review not found" });
+    }
+    return res.status(200).json(review);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+// GET Review by UserId
+router.get("/:userId", async (req, res) => {
+  try {
+    const userReviews = await readEntryByAggerate("Review", {
+      where: {
+        userid: req.params.userId,
+      },
+    });
+    if (!userReviews) {
+      return res.status(404).json({ message: "User has no reviews" });
+    }
+    return res.status(200).json(userReviews);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Sever Error" });
+  }
+});
 module.exports = router;
