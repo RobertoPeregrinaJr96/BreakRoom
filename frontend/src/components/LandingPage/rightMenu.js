57333aeb9807af282d36a41ac60d55a8117e7116
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-03-09 18:01:31 -0800

    updated logic to menu component

--- frontend/src/components/LandingPage/rightMenu.js
+++ frontend/src/components/LandingPage/rightMenu.js
@@ -1,9 +1,32 @@
+import "./style/rightMenu.css";
+
 function RightMenu() {
+
+  let menuItems = [{ name: "hello" }];
+
   return (
     <>
-      <h1>Right Menu</h1>
+      <h1 className="rightMenu-h1">Right Menu</h1>
       <div className="rightMenu-container">
-        <menu></menu>
+        <menu className="menu-item-container">
+          {menuItems ? (
+            <span className="menu-item-cluster">
+              <button>Item name</button>
+              <button>Item info link</button>
+              <button>Item module button</button>
+              <button>Item add to cart</button>
+            </span>
+          ) : (
+            menuItems.forEach((item) => {
+              <span className="menu-item-cluster">
+                <button>{item.name}</button>
+                <button>img</button>
+                <button>img</button>
+                <button>img</button>
+              </span>;
+            })
+          )}
+        </menu>
       </div>
     </>
   );
