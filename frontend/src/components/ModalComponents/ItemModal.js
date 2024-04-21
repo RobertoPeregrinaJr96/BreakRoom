71fa22b501655006c8a742513a20add5c433e6e5
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-04-21 15:16:25 -0700

    modal is okay for now on menu

--- frontend/src/components/ModalComponents/ItemModal.js
+++ frontend/src/components/ModalComponents/ItemModal.js
@@ -13,23 +13,27 @@ const ItemModel = ({ item }) => {
   return (
     <>
       <div className="modal-item-div-wrapper">
-        <div className="modal-item-button-container">
-          <p>{item.name}</p>
-          <p>{item.description}</p>
-
-          <div className="modal-item-buttons">
-            <button
-              className="modal-item-button-cancel"
-              onClick={() => closeModal()}
-            >
-              Cancel
-            </button>
-            <button
-              className="modal-item-button-submit"
-              onClick={() => closeModal()}
-            >
-              Submit
-            </button>
+        <div>
+          <span className="modal-item-info-container">
+            <p>{item.name}</p>
+            <p>$ {item.price}</p>
+          </span>
+          <p className="modal-item-description">{item.description}</p>
+          <div className="modal-item--container">
+            <div className="modal-item-buttons">
+              <button
+                className="modal-item-button-cancel"
+                onClick={() => closeModal()}
+              >
+                Cancel
+              </button>
+              <button
+                className="modal-item-button-submit"
+                onClick={() => closeModal()}
+              >
+                Submit
+              </button>
+            </div>
           </div>
         </div>
       </div>
