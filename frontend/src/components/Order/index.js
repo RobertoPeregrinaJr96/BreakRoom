dbff78a8a247921f90f7683a0e5cb62969d6e50a
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-05-08 18:26:16 -0700

    order logic/ css update

--- frontend/src/components/Order/index.js
+++ frontend/src/components/Order/index.js
@@ -1,5 +1,5 @@
 // frontend/src/components/Navigation/index.js
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import "./style/order.css";
@@ -7,23 +7,55 @@ import { getCurrentOrderByIdThunk } from "../../store/order";
 function OrderPage() {
   const dispatch = useDispatch();
   const order = useSelector((state) => state.order);
-  console.log("Order: ", order);
   const orderItems = order?.currentOrder["orderItems"]
     ? order?.currentOrder["orderItems"]
     : [];
-  console.log("Order Items:", orderItems);
+  const [displayPrefference, setDisplayPrefference] = useState("grid");
+  const [total, setTotal] = useState(order["tot"]);
+  const itemTotalPrice = (item, food) => {
+    let sum = 0;
+    return (sum += item.quantity * food.price);
+  };
+
+  const selectPrefference = (e) => {
+    e.preventDefault();
+    if (displayPrefference === "grid") {
+      setDisplayPrefference("block");
+    } else {
+      setDisplayPrefference("grid");
+    }
+  };
+
   useEffect(() => {
     dispatch(getCurrentOrderByIdThunk());
   }, [dispatch]);
+
   return order ? (
     <>
       <h1>OrderPage</h1>
-      <ul>
+      <button
+        className="order-item-layout"
+        onClick={(e) => selectPrefference(e)}
+      ></button>
+      <ul className={`order-item-container-${displayPrefference}`}>
         {orderItems.map((item) => {
+          const food = item.item;
+          const modifiers = item.modifiers;
           return (
-            <li key={item.id}>
-              <span> {item.item["name"]}</span>
-              <div></div>
+            <li key={item.id} className="order-item-li">
+              <span className={`order-item-info-${displayPrefference}`}>
+                <span>
+                  <h3>
+                    {" "}
+                    {food["name"]} ${itemTotalPrice(item, food)}
+                  </h3>
+                  <p>
+                    {modifiers.map((a) => ` ${a.modifierName} total price`)}
+                  </p>
+                </span>
+                <p>{item["customInstruction"].slice(0, 20)}</p>
+              </span>
+              <div className={`order-item-update-${displayPrefference}`}></div>
             </li>
           );
         })}
