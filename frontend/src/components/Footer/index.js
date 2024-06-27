88817d25a51b270f51be9225d4f221877bea16e5
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-06-27 14:47:26 -0700

    footer (dark mode/light mode) mvp temp

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,40 +1,96 @@
 // frontend/src/components/Navigation/index.js
 
 import "./style/footer.css";
+import { useSelector } from "react-redux";
 
 function Footer() {
-  return (
-    <>
-      <div className="Footer">
-        <ul className="footer-list">
-          <li className="footer-item">Developer : Roberto Peregrina Jr</li>
-          <li className="footer-item">
-            <a target="_" href="https://www.linkedin.com/in/roberto-peregrina/">
-              Linkedin:<i class="fa-brands fa-linkedin"></i>
-            </a>
-          </li>
-          <li className="footer-item">
-            <a target="_" href="https://github.com/RobertoPeregrinaJr96">
-              Github:<i class="fa-brands fa-github"></i>
-            </a>
-          </li>
-          <li className="footer-item">
-            <a
-              target="_"
-              href="https://github.com/RobertoPeregrinaJr96/BreakRoom"
-            >
-              Github Repository:<i class="fa-brands fa-github"></i>
-            </a>
-          </li>
-          <li className="footer-item">
-            <a target="_" href="https://robertoperegrinajr96.github.io/#">
-              Portfolio:
-            </a>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
+  const displaySettings = useSelector((state) => state.session.setting);
+  let display = displaySettings.display;
+  let mode = displaySettings.mode;
+
+  // CSS logic
+
+  const darkMode = (mode) => {
+    return (
+      <>
+        <div className={`Footer-${mode}`}>
+          <ul className="footer-list">
+            <li className="footer-item">Developer : Roberto Peregrina Jr</li>
+            <li className="footer-item">
+              <a
+                target="_"
+                href="https://www.linkedin.com/in/roberto-peregrina/"
+              >
+                Linkedin:<i class="fa-brands fa-linkedin"></i>
+              </a>
+            </li>
+            <li className="footer-item">
+              <a target="_" href="https://github.com/RobertoPeregrinaJr96">
+                Github:<i class="fa-brands fa-github"></i>
+              </a>
+            </li>
+            <li className="footer-item">
+              <a
+                target="_"
+                href="https://github.com/RobertoPeregrinaJr96/BreakRoom"
+              >
+                Github Repository:<i class="fa-brands fa-github"></i>
+              </a>
+            </li>
+            <li className="footer-item">
+              <a target="_" href="https://robertoperegrinajr96.github.io/#">
+                Portfolio:
+              </a>
+            </li>
+          </ul>
+        </div>
+      </>
+    );
+  };
+
+  const lightMode = (mode) => {
+    return (
+      <>
+        <div className={`Footer-${mode}`}>
+          <ul className="footer-list">
+            <li className="footer-item">Developer : Roberto Peregrina Jr</li>
+            <li className="footer-item">
+              <a
+                target="_"
+                href="https://www.linkedin.com/in/roberto-peregrina/"
+              >
+                Linkedin:<i class="fa-brands fa-linkedin"></i>
+              </a>
+            </li>
+            <li className="footer-item">
+              <a target="_" href="https://github.com/RobertoPeregrinaJr96">
+                Github:<i class="fa-brands fa-github"></i>
+              </a>
+            </li>
+            <li className="footer-item">
+              <a
+                target="_"
+                href="https://github.com/RobertoPeregrinaJr96/BreakRoom"
+              >
+                Github Repository:<i class="fa-brands fa-github"></i>
+              </a>
+            </li>
+            <li className="footer-item">
+              <a target="_" href="https://robertoperegrinajr96.github.io/#">
+                Portfolio:
+              </a>
+            </li>
+          </ul>
+        </div>
+      </>
+    );
+  };
+
+  if (mode === "dark") {
+    return darkMode(mode);
+  } else {
+    return lightMode(mode);
+  }
 }
 
 export default Footer;
