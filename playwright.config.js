e04b7635c9bde3954328b4c8c1e59142cedc5a73
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-08-10 18:54:38 -0700

    config updated

--- playwright.config.js
+++ playwright.config.js
@@ -1,31 +1,21 @@
-// playwright.config.js
 const { devices } = require("@playwright/test");
 
 const frontendConfig = {
   testDir: "./test/frontend",
-  // untill timeout / failure
   timeout: 30000,
-  // expectation(assertions)
   expect: {
-    // max time to wait for assertions
     timeout: 5000,
   },
-  // display the results
   reporter: "html",
-  // settings
   use: {
-    // Base URL to use in actions like await page.goto('/').
     baseURL: "http://127.0.0.1:3000",
-    // run test without opening the browser
     headless: true,
-    // ignore errors
     ignoreHTTPSErrors: true,
-    // error logs for failed test
     trace: "on-first-retry",
   },
   projects: [
     {
-      name: "chromium",
+      name: "frontend-chromium",
       use: { ...devices["Desktop Chrome"] },
     },
   ],
@@ -33,35 +23,29 @@ const frontendConfig = {
 
 const backendConfig = {
   testDir: "./test/backend",
-  // untill timeout / failure
   timeout: 30000,
-  // expectation(assertions)
   expect: {
-    // max time to wait for assertions
     timeout: 5000,
   },
-  // display the results
   reporter: "html",
-  // settings
   use: {
-    // Base URL to use in actions like await page.goto('/').
     baseURL: "http://127.0.0.1:8000",
-    // run test without opening the browser
     headless: true,
-    // ignore errors
     ignoreHTTPSErrors: true,
-    // error logs for failed test
     trace: "on-first-retry",
   },
   projects: [
     {
-      name: "chromium",
+      name: "backend-chromium",
       use: { ...devices["Desktop Chrome"] },
     },
   ],
 };
 
-const selectedConfig =
-  process.env.TEST_ENV === "backend" ? backendConfig : frontendConfig;
-
-module.exports = selectedConfig;
+module.exports = {
+  // Merge both configs under one Playwright configuration
+  projects: [
+    frontendConfig.projects[0], // Frontend Project
+    backendConfig.projects[0],  // Backend Project
+  ],
+};
